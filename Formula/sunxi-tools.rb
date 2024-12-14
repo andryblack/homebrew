03036b0fba517c2062bb57c0ed56c8dbd5d9b81f
andryblack <blackicebox@gmail.com>
2024-12-14 23:17:42 +0300

    master with patch

--- Formula/sunxi-tools.rb
+++ Formula/sunxi-tools.rb
@@ -3,19 +3,22 @@
 class SunxiTools < Formula
   desc ""
   homepage "https://linux-sunxi.org/Sunxi-tools"
-  url "https://github.com/linux-sunxi/sunxi-tools.git", tag: "v1.4.2", revision: "85943dfa8f5cc6613a65f622801d3cd951efbf6a"
-  version "1.4.2"
+  url "https://github.com/linux-sunxi/sunxi-tools.git", branch: "master", revision: "29d48c3c39d74200fb35b5750f99d06a4886bf2e"
+  version "master"
   license "GPL-2.0"
 
   depends_on "dtc" => :build
   depends_on "libusb" => :build
+  depends_on "coreutils" => :build
+
+  patch :DATA
 
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     #system "./configure", "--disable-silent-rules", *std_configure_args
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "make", "PREFIX=#{prefix}", "install"
+    system "make", "PREFIX=#{prefix}", "INSTALL=ginstall", "install"
   end
 
   test do
@@ -31,3 +34,47 @@ class SunxiTools < Formula
     system "false"
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index 3d89718..d8bd20a 100644
+--- a/Makefile
++++ b/Makefile
+@@ -65,6 +65,7 @@ DESTDIR ?=
+ PREFIX  ?= /usr/local
+ BINDIR  ?= $(PREFIX)/bin
+ MANDIR  ?= $(PREFIX)/share/man/man1
++INSTALL ?= install
+ 
+ .PHONY: all clean tools target-tools install install-tools install-target-tools
+ .PHONY: check
+@@ -84,23 +85,23 @@ install-all: install-tools install-target-tools
+ install-tools: $(TOOLS)
+ 	install -d $(DESTDIR)$(BINDIR)
+ 	@set -ex ; for t in $^ ; do \
+-		install -m0755 $$t $(DESTDIR)$(BINDIR)/$$t ; \
++		$(INSTALL) -m0755 $$t $(DESTDIR)$(BINDIR)/$$t ; \
+ 	done
+ 	@set -ex ; for l in $(FEXC_LINKS) ; do \
+ 		ln -nfs sunxi-fexc $(DESTDIR)$(BINDIR)/$$l ; \
+ 	done
+-	install -D -m0644 -t $(DESTDIR)$(MANDIR) sunxi-fel.1
++	$(INSTALL) -D -m0644 -t $(DESTDIR)$(MANDIR) sunxi-fel.1
+ 
+ install-target-tools: $(TARGET_TOOLS)
+-	install -d $(DESTDIR)$(BINDIR)
++	$(INSTALL) -d $(DESTDIR)$(BINDIR)
+ 	@set -ex ; for t in $^ ; do \
+-		install -m0755 $$t $(DESTDIR)$(BINDIR)/$$t ; \
++		$(INSTALL) -m0755 $$t $(DESTDIR)$(BINDIR)/$$t ; \
+ 	done
+ 
+ install-misc: $(MISC_TOOLS)
+-	install -d $(DESTDIR)$(BINDIR)
++	$(INSTALL) -d $(DESTDIR)$(BINDIR)
+ 	@set -ex ; for t in $^ ; do \
+-		install -m0755 $$t $(DESTDIR)$(BINDIR)/$$t ; \
++		$(INSTALL) -m0755 $$t $(DESTDIR)$(BINDIR)/$$t ; \
+ 	done
+ 
+ 
